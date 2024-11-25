4caa9e813a8a89962b7c49733dbce15d9273a611
ElmerPicado <epicadomiranda@gmail.com>
2024-11-25 02:26:03 -0600

    examen

---
+++ clasesProgra2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clasesProgra2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            entrada();
+            Clicofor();
+            
+        }
+        static void entrada()
+        {
+            string nombre = "";
+            Console.WriteLine("HOLA");
+            Console.WriteLine("CUAL ES TU NOMRBE");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola :" + nombre);
+           
+
+        }
+
+        static void Clicofor()
+        {
+            for (int i = 10; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+        
+       
+    }
+}
